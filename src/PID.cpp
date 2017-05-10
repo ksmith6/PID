4b2f89b6d8cdd2bcab38171c9a027e7f219a2c87
Kelly Smith <ksmith433@gatech.edu>
2017-05-09 22:22:38 -0500

    Added PID controller implementation as well as tuned controller gains.

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,5 @@
 #include "PID.h"
-
+#include <iostream>
 using namespace std;
 
 /*
@@ -11,11 +11,32 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
+
+	Kp_ = Kp;
+	Ki_ = Ki;
+	Kd_ = Kd;
+
 }
 
 void PID::UpdateError(double cte) {
+
+	//  // The derivative of the error is simply the difference between the current error and the previous error.
+	d_error = cte - p_error;
+
+	// The proportional error is simply the current crosstrack error (CTE)
+	p_error = cte;
+
+	// The integral error term is simply the running sum of crosstrack errors (CTE)
+	i_error += cte;
+
+	std::cout << "P = " << d_error << " | I = " << i_error << " | D = " << d_error << std::endl;
+
 }
 
 double PID::TotalError() {
+	return Kp_*p_error + Ki_*i_error + Kd_*d_error;
 }
 
